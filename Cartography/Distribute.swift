f0dcea1b2210d2a6f2d6a41e1e00f26ffdb20fca
Paul Rolfe <paul.a.rolfe@gmail.com>
2016-01-14 17:56:14 -0500

    Distribute evenly that takes [LayoutProxy]. For all distribution varieties (H, Z, L to R)

--- Cartography/Distribute.swift
+++ Cartography/Distribute.swift
@@ -38,6 +38,24 @@ typealias Accumulator = ([NSLayoutConstraint], LayoutProxy)
     return reduce(first, rest: rest) { $0.trailing == $1.leading - amount }
 }
 
+/// Distributes multiple views horizontally. And makes them all the same width.
+///
+/// All views passed to this function will have
+/// their `translatesAutoresizingMaskIntoConstraints` properties set to `false`.
+///
+/// - parameter amount: The distance between the views.
+/// - parameter views:  The views to distribute.
+///
+/// - returns: An array of `NSLayoutConstraint` instances.
+///
+@discardableResult public func distributeEvenly(by amount: CGFloat, horizontally all: [LayoutProxy]) -> [NSLayoutConstraint] {
+    guard let first = all.first else { fatalError("You need at least one LayoutProxy in the array") }
+    let rest = Array(all.dropFirst())
+    let spacing = reduce(first, rest: rest) { $0.trailing == $1.leading - amount }
+    let even = reduce(first, rest: rest) { $0.width == $1.width }
+    return spacing + even
+}
+
 /// Distributes multiple views horizontally from left to right.
 ///
 /// All views passed to this function will have
@@ -52,6 +70,24 @@ typealias Accumulator = ([NSLayoutConstraint], LayoutProxy)
     return reduce(first, rest: rest) { $0.right == $1.left - amount  }
 }
 
+/// Distributes multiple views horizontally from left to right. And makes them all the same width.
+///
+/// All views passed to this function will have
+/// their `translatesAutoresizingMaskIntoConstraints` properties set to `false`.
+///
+/// - parameter amount: The distance between the views.
+/// - parameter views:  The views to distribute.
+///
+/// - returns: An array of `NSLayoutConstraint` instances.
+///
+@discardableResult public func distributeEvenly(by amount: CGFloat, leftToRight all: [LayoutProxy]) -> [NSLayoutConstraint] {
+    guard let first = all.first else { fatalError("You need at least one LayoutProxy in the array") }
+    let rest = Array(all.dropFirst())
+    let spacing = reduce(first, rest: rest) { $0.right == $1.left - amount }
+    let even = reduce(first, rest: rest) { $0.width == $1.width }
+    return spacing + even
+}
+
 /// Distributes multiple views vertically.
 ///
 /// All views passed to this function will have
@@ -64,4 +100,22 @@ typealias Accumulator = ([NSLayoutConstraint], LayoutProxy)
 ///
 @discardableResult public func distribute(by amount: CGFloat, vertically first: LayoutProxy, _ rest: LayoutProxy...) -> [NSLayoutConstraint] {
     return reduce(first, rest: rest) { $0.bottom == $1.top - amount }
+}
+
+/// Distributes multiple views vertically. And makes them all the same height
+///
+/// All views passed to this function will have
+/// their `translatesAutoresizingMaskIntoConstraints` properties set to `false`.
+///
+/// - parameter amount: The distance between the views.
+/// - parameter views:  The views to distribute.
+///
+/// - returns: An array of `NSLayoutConstraint` instances.
+///
+@discardableResult public func distributeEvenly(by amount: CGFloat, vertically all: [LayoutProxy]) -> [NSLayoutConstraint] {
+    guard let first = all.first else { fatalError("You need at least one LayoutProxy in the array") }
+    let rest = Array(all.dropFirst())
+    let spacing = reduce(first, rest: rest) { $0.bottom == $1.top - amount }
+    let even = reduce(first, rest: rest) { $0.height == $1.height }
+    return spacing + even
 }
